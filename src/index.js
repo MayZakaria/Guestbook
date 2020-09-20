17be850fe13bcd0211f7ffd0e22b3fd8c0a220c1
mai zakaria <mai.zakaria.stdsw@cis.asu.edu.eg>
2020-09-20 10:13:14 +0200

    react init

--- src/index.js
+++ src/index.js
@@ -3,10 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { applyMiddleware, createStore } from 'redux';
+import reduxThunk from 'redux-thunk';
+import { rootReducer } from './reducers';
+import { Provider } from 'react-redux';
+
+const createStoreWithMiddleware =applyMiddleware(reduxThunk)(createStore);
+
+const myStore= createStoreWithMiddleware(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={myStore}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
